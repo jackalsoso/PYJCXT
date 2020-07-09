26c87b6b8cb8341d3fe10049ffe38e83a4236aea
yiming_shou@ssic.cn <yiming_shou@ssic.cn>
2020-07-09 18:31:17 +0800

    更新

--- js/common.js
+++ js/common.js
@@ -2,5 +2,11 @@ $(function(){
   $('.themes-picker').on('click', 'li', function(){
     let _theme = $(this).attr('data-theme')
     $('#cssthemes').attr('href', './css/' + _theme + '.css')
+    $(this).addClass('active').siblings().removeClass('active')
   })
+  $('.page-nav').on('click', 'ul li', function(){
+    let _url = $(this).attr('data-menu')
+    window.location.href = './' + _url + '.html'
+  })
+
 })
