48ddb9e19690451597ca40cf2065ad801127fa0d
yiming_shou@ssic.cn <yiming_shou@ssic.cn>
2020-07-10 00:37:05 +0800

    更新

--- js/common.js
+++ js/common.js
@@ -1,7 +1,9 @@
 $(function(){
+  !sessionStorage.theme ? sessionStorage.setItem('theme','dark') : ''
+  $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
   $('.themes-picker').on('click', 'li', function(){
-    let _theme = $(this).attr('data-theme')
-    $('#cssthemes').attr('href', './css/' + _theme + '.css')
+    sessionStorage.theme = $(this).attr('data-theme')
+    $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
     $(this).addClass('active').siblings().removeClass('active')
   })
   $('.page-nav').on('click', 'ul li', function(){
