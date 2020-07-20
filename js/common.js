31717111d798924888ebfcc39a6cb69e5ff433ef
yiming_shou@ssic.cn <yiming_shou@ssic.cn>
2020-07-20 14:12:52 +0800

    更新

--- js/common.js
+++ js/common.js
@@ -1,11 +1,12 @@
 $(function(){
   !sessionStorage.theme ? sessionStorage.setItem('theme','dark') : ''
   $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
+  $('.themes-picker').attr('data-theme', sessionStorage.theme)
   init ? setTimeout(function(){init()},100) : ''
-  $('.themes-picker').on('click', 'li', function(){
+  $('.themes-picker').on('click', function(){
+    sessionStorage.theme == 'normal' ? $(this).attr('data-theme', 'dark') : $(this).attr('data-theme', 'normal')
     sessionStorage.theme = $(this).attr('data-theme')
     $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
-    $(this).addClass('active').siblings().removeClass('active')
   })
   $('.page-nav, .time-top').on('click', 'li', function(e){
     let _url = $(this).attr('data-menu')
