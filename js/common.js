77eb77d1c3ffd76e2a9adc0da07430d476037229
su sheng jie <susj@mti-sh.cn>
2020-07-25 02:29:19 +0800

    添加列表及白版图表

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $(function(){
   !sessionStorage.theme ? sessionStorage.setItem('theme','dark') : ''
   $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
   $('.themes-picker').attr('data-theme', sessionStorage.theme)
-  init ? setTimeout(function(){init()},100) : ''
+  init ? setTimeout(function(){init()},500) : ''
   $('.themes-picker').on('click', function(){
     sessionStorage.theme == 'normal' ? $(this).attr('data-theme', 'dark') : $(this).attr('data-theme', 'normal')
     sessionStorage.theme = $(this).attr('data-theme')
