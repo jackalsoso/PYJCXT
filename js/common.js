539136c3b4491c4f54973257cdc5e38debc85f3e
yiming_shou@ssic.cn <yiming_shou@ssic.cn>
2020-09-14 07:14:05 +0800

    更新

--- js/common.js
+++ js/common.js
@@ -3,11 +3,15 @@ $(function(){
   $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
   $('.themes-picker').attr('data-theme', sessionStorage.theme)
   init ? setTimeout(function(){init()},500) : ''
-  $('.themes-picker').on('click', function(){
-    sessionStorage.theme == 'normal' ? $(this).attr('data-theme', 'dark') : $(this).attr('data-theme', 'normal')
-    sessionStorage.theme = $(this).attr('data-theme')
-    $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
-    init ? init() : ''
+  $('.themes-picker').on('click', 'li',  function(){
+    if(sessionStorage.theme != $(this).attr('data-theme')){
+      $(this).addClass('active').siblings().removeClass('active')
+      sessionStorage.theme = $(this).attr('data-theme')
+      $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
+      init ? init() : ''
+
+    }
+    // sessionStorage.theme = $(this).attr('data-theme')
   })
   $('.page-nav, .time-top').on('click', 'li', function(e){
     let _url = $(this).attr('data-menu')
