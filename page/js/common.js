d4bc08fd4f49b259d5b77d610ef5211c9b3a059a
819966041 <38412190+819966041@users.noreply.github.com>
2020-09-29 18:00:29 +0800

    修改第7版页面样式，新增unity逻辑判定，切换皮肤改变三维场景颜色

--- page/js/common.js
+++ page/js/common.js
@@ -1,21 +1,34 @@
-$(function(){
-  !sessionStorage.theme ? sessionStorage.setItem('theme','dark') : ''
+$(function () {
+  !sessionStorage.theme ? sessionStorage.setItem('theme', 'dark') : ''
   $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
   $('.themes-picker').attr('data-theme', sessionStorage.theme)
-  init ? setTimeout(function(){init()},500) : ''
-  $('.themes-picker').on('click', 'li',  function(){
-    if(sessionStorage.theme != $(this).attr('data-theme')){
+  init ? setTimeout(function () { init() }, 500) : ''
+  $('.themes-picker').on('click', 'li', function (event) {
+    console.log(event.currentTarget.className == "blue")
+    console.log(event.currentTarget.className == "white")
+    // debugger
+    window.localStorage.setItem("color", event.currentTarget.className)
+      
+
+    if (sessionStorage.theme != $(this).attr('data-theme')) {
       $(this).addClass('active').siblings().removeClass('active')
       sessionStorage.theme = $(this).attr('data-theme')
       $('#cssthemes').attr('href', './css/' + sessionStorage.theme + '.css')
       init ? init() : ''
 
     }
+    if (event.currentTarget.className == "white") {
+      unity.SendMessage("GameManager", "SetBgColor", "#234780")//
+      unity.SendMessage("GameManager", "SetTransColor", "#6C6767;0.26")//
+    } else {
+      unity.SendMessage("GameManager", "SetBgColor", "#051631")//
+      unity.SendMessage("GameManager", "SetTransColor", "#FFFFFF;0.1")//
+    }
     // sessionStorage.theme = $(this).attr('data-theme')
   })
-  $('.page-nav, .time-top').on('click', 'li', function(e){
+  $('.page-nav, .time-top').on('click', 'li', function (e) {
     let _url = $(this).attr('data-menu')
-    if(_url){
+    if (_url) {
       e.stopPropagation()
       let _url = $(this).attr('data-menu')
       window.location.href = './' + _url + '.html'
